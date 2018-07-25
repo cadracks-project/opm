098083e18f165404a69fc4563e8d835b2f863077
Guillaume Florent <guillaume.florent@tutanota.com>
2018-07-25 13:50:25 +0200

    Updated db file

---
+++ opm/api.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+
+# #######
+# Write #
+# #######
+
+
+def create_assembly(product_code, assembly_code):
+    raise NotImplementedError
+
+
+def add_part(product_code, assembly_code, part):
+    raise NotImplementedError
+
+# ##########
+# Read API #
+# ##########
+
+
+def product_shape(product_code):
+    raise NotImplementedError
